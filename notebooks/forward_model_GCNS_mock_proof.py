13d0a78d68463418d3ade3bf5ca7e5e7217de5da
imedan <ilija.medan@gmail.com>
2025-12-15 11:37:40 -0600

    fix path for accre

--- notebooks/forward_model_GCNS_mock_proof.py
+++ notebooks/forward_model_GCNS_mock_proof.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     n_g_rp = len(np.arange(*sf_bins['g_rp'])) - 1
     n_mg = len(np.arange(*MG_bin_list)) - 1
 
-    data_file = '../../obs_100pc_edr3.csv'
+    data_file = 'obs_100pc_edr3.csv'
 
     # initilize
     mean = False
